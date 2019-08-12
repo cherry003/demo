e85ae88a9e5d6a7801eabb853e9db81ae7f8600a
sammiww03 <44799777+sammiww03@users.noreply.github.com>
2019-08-12 16:07:02 +0800

    Update hellow.ps1

--- hellow.ps1
+++ hellow.ps1
@@ -6,4 +6,8 @@ Write-Host "Hello world 123" from $greeter
 Write-Host Trigger: $trigger
 Start-Sleep -s 60
 Write-Host "Now that you've got CI/CD, you can automatically deploy your app every time your team checks in code.WELCOME HERE"
-Write-Host "Hello world 1dddd7773"
+Write-Host "Hello world cherry"
+
+Write-Host "Hello world cherry"
+
+Write-Host "Hello world cherry"
