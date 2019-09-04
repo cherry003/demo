5a0abd87f500f53efaef5dd5004a1132cded1508
cherry003 <42259332+cherry003@users.noreply.github.com>
2019-09-04 11:43:28 +0800

    JIRA update file

--- hellow.ps1
+++ hellow.ps1
@@ -7,5 +7,5 @@ Write-Host Trigger: $trigger
 Write-Host "Now that you've got CI/CD, you can automatically deploy your app every time your team checks in code.WELCOME HERE"
 Write-Host "Hello world cherry"
 Write-Host "Hello world cherry"
-
+Write-Host "Hello world cherry"
 
