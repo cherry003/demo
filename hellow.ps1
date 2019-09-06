31b26e0dfe296f325025628ae47e1136e93fc781
cherry003 <42259332+cherry003@users.noreply.github.com>
2019-09-06 10:16:07 +0800

    JIRA-2 UPDATE

--- hellow.ps1
+++ hellow.ps1
@@ -2,8 +2,8 @@ Param(
 [string]$greeter,
 [string]$trigger
 )
-Write-Host "Hello world 123" from $greeter
-Write-Host Trigger: $trigger
+Write-Host "Build.RequestedFor": $greeter
+Write-Host "Build.QueuedBy": $trigger
 Write-Host "Now that you've got CI/CD, you can automatically deploy your app every time your team checks in code.WELCOME HERE"
 Write-Host "Hello world cherry"
 Write-Host "Hello world cherry"
